973b1039b44562174af57723e36d935a85cc61b9
Xiorent Technologies <xiorentworld@gmail.com>
2025-10-31 19:18:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,54 @@
 import express from "express";
-import userRoutes from "./routes/userRoutes.js"
-import hotelRoutes from "./routes/hotelRoute.js"
-import roomRoutes from "./routes/roomRoute.js"
-import bookingRoutes from "./routes/BookingRoutes.js"
-import vacancyRoutes from "./routes/VacancyRoutes.js"
-import refundRoutes from "./routes/refundRoute.js"
-import cors from "cors"
+import userRoutes from "./routes/userRoutes.js";
+import hotelRoutes from "./routes/hotelRoute.js";
+import roomRoutes from "./routes/roomRoute.js";
+import bookingRoutes from "./routes/BookingRoutes.js";
+import vacancyRoutes from "./routes/VacancyRoutes.js";
+import refundRoutes from "./routes/refundRoute.js";
+import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./db/connectDB.js";
 import cookieParser from "cookie-parser";
 
-
-const app = express()
-
+const app = express();
 
 dotenv.config();
 connectDB();
 
-
 const PORT = process.env.PORT || 5000;
-app.use(express.json({limit: "10mb"}));
+
+app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
+
+
+const allowedOrigins = [
+  "https://hotelfei.vercel.app",
+  "http://localhost:5173", // for local development
+];
 
 app.use(
   cors({
-    origin: "https://hotelfei.vercel.app/", // frontend Vite URL
+    origin: function (origin, callback) {
+      // allow requests with no origin (like mobile apps or curl)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
   })
 );
 
+app.use("/api/auth", userRoutes);
+app.use("/api/hotel", hotelRoutes);
+app.use("/api/room", roomRoutes);
+app.use("/api/bookings", bookingRoutes);
+app.use("/api/vacancy", vacancyRoutes);
+app.use("/api/refund", refundRoutes);
 
-
-app.use("/api/auth",userRoutes);
-app.use("/api/hotel",hotelRoutes);
-app.use("/api/room",roomRoutes);
-app.use("/api/bookings",bookingRoutes)
-app.use("/api/vacancy",vacancyRoutes);
-app.use("/api/refund",refundRoutes)
-
-app.listen(PORT,() => {
-    console.log(`Server is running on port ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
